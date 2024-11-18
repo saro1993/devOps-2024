13934c300b3b6185f3b3fb78b0fee33563aa6fcf
saro1993 <sarosamall@yahoo.com>
2024-11-18 16:11:31 +0000

    updated infra folder

--- infra-tr/outputs.tf
+++ infra-tr/outputs.tf
@@ -4,12 +4,13 @@ output "password" {
 
 output "lambda_function_arn" {
   value = aws_lambda_function.image_processor_lambda.arn
+  description = "Lambda function "
 }
 
 output "sqs_queue_url" {
   value = aws_sqs_queue.image_queue.url
+  description = "URL til SQS-køen"
 }
 output "iam_student_user_name" {
   value = aws_iam_user.student.name
-  description = "Navnet på IAM-brukeren som ble opprettet"
 }
