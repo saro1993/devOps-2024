338be489d0125f363ab90736a328322f5666680f
saro1993 <sarosamall@yahoo.com>
2024-11-18 12:22:01 +0000

    updated infra folder

--- infra-tr/sqs/sqs.tf
+++ infra-tr/sqs/sqs.tf
@@ -1,5 +1,5 @@
 resource "aws_sqs_queue" "image_queue" {
   name = "image_processing_queue"
-  message_retention_seconds = 345600  # 4 dager
+  message_retention_seconds = 345600
   visibility_timeout_seconds = 30
 }
